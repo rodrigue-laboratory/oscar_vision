133c50ab12fb06b68b6d13b2a039ee9c81f28177
Captain Yoshi <captain.yoshisaur@gmail.com>
2024-10-04 07:08:26 -0400

    [impl] auto exposure roi

--- include/mimik/vision/realsense.h
+++ include/mimik/vision/realsense.h
@@ -17,10 +17,13 @@ public:
   bool disableStreamingSensors();
 
   bool resetCamera();
-  sensor_msgs::CameraInfo::ConstPtr getRGBCameraInfo();
-  sensor_msgs::CameraInfo::ConstPtr getDepthCameraInfo();
   sensor_msgs::Image::ConstPtr getRGBImage();
   sensor_msgs::Image::ConstPtr getDepthImage();
+  sensor_msgs::CameraInfo::ConstPtr getRGBCameraInfo();
+  sensor_msgs::CameraInfo::ConstPtr getDepthCameraInfo();
+
+  bool setRGBAutoExposureROI(int left, int right, int top, int bottom);
+  bool setDepthAutoExposureROI(int left, int right, int top, int bottom);
 
 private:
   void RGBImageCallback(const sensor_msgs::Image::ConstPtr& msg);
@@ -28,13 +31,15 @@ private:
   void RGBCameraInfoCallback(const sensor_msgs::CameraInfo::ConstPtr& msg);
   void DepthCameraInfoCallback(const sensor_msgs::CameraInfo::ConstPtr& msg);
 
+  // Service Client
   std::shared_ptr<ros::ServiceClient> service_client_reset_camera_;
   std::shared_ptr<ros::ServiceClient> service_client_enable_stream_;
 
-  std::shared_ptr<ros::Subscriber> subscriber_rgb_camera_info_;
-  std::shared_ptr<ros::Subscriber> subscriber_depth_camera_info_;
+  // Subscriber/Publisher
   std::shared_ptr<ros::Subscriber> subscriber_rgb_image_;
   std::shared_ptr<ros::Subscriber> subscriber_depth_image_;
+  std::shared_ptr<ros::Subscriber> subscriber_rgb_camera_info_;
+  std::shared_ptr<ros::Subscriber> subscriber_depth_camera_info_;
 
   sensor_msgs::Image::ConstPtr rgb_image_;
   sensor_msgs::Image::ConstPtr depth_image_;
@@ -45,6 +50,9 @@ private:
   std::mutex mutex_depth_image_;
   std::mutex mutex_rgb_camera_info_;
   std::mutex mutex_depth_camera_info_;
+
+  std::string topic_rgb_roi_;
+  std::string topic_depth_roi_;
 };
 
 }  // namespace vision
--- src/realsense.cpp
+++ src/realsense.cpp
@@ -4,6 +4,9 @@
 
 #include <sensor_msgs/Image.h>
 
+#include <dynamic_reconfigure/Reconfigure.h>
+#include <dynamic_reconfigure/IntParameter.h>
+
 namespace mimik {
 namespace vision {
 
@@ -28,6 +31,9 @@ RealSenseCamera::RealSenseCamera(ros::NodeHandle& nh, const std::string& namespa
       nh.subscribe(namespace_prefix + "/color/image_raw", 1, &RealSenseCamera::RGBImageCallback, this));
   subscriber_depth_image_ = std::make_shared<ros::Subscriber>(
       nh.subscribe(namespace_prefix + "/depth/image_rect_raw", 1, &RealSenseCamera::DepthImageCallback, this));
+
+  topic_rgb_roi_ = namespace_prefix + "/rgb_camera/auto_exposure_roi/set_parameters";
+  topic_depth_roi_ = namespace_prefix + "/stereo_module/auto_exposure_roi/set_parameters";
 }
 
 void RealSenseCamera::RGBImageCallback(const sensor_msgs::Image::ConstPtr& msg)
@@ -117,6 +123,80 @@ sensor_msgs::Image::ConstPtr RealSenseCamera::getDepthImage()
 {
   const std::lock_guard<std::mutex> lock(mutex_depth_image_);
   return depth_image_;
+}
+
+bool RealSenseCamera::setRGBAutoExposureROI(int left, int right, int top, int bottom)
+{
+  dynamic_reconfigure::ReconfigureRequest req;
+  dynamic_reconfigure::ReconfigureResponse res;
+
+  dynamic_reconfigure::IntParameter left_param;
+  dynamic_reconfigure::IntParameter right_param;
+  dynamic_reconfigure::IntParameter top_param;
+  dynamic_reconfigure::IntParameter bottom_param;
+
+  left_param.name = "left";
+  left_param.value = left;
+
+  right_param.name = "right";
+  right_param.value = right;
+
+  top_param.name = "top";
+  top_param.value = top;
+
+  bottom_param.name = "bottom";
+  bottom_param.value = bottom;
+
+  auto& config = req.config;
+  config.ints.push_back(left_param);
+  config.ints.push_back(right_param);
+  config.ints.push_back(top_param);
+  config.ints.push_back(bottom_param);
+
+  if (!ros::service::call(topic_rgb_roi_, req, res))
+  {
+    ROS_ERROR("Failed to rconfigure the RGB auto exposure ROI");
+    return false;
+  }
+
+  return true;
+}
+
+bool RealSenseCamera::setDepthAutoExposureROI(int left, int right, int top, int bottom)
+{
+  dynamic_reconfigure::ReconfigureRequest req;
+  dynamic_reconfigure::ReconfigureResponse res;
+
+  dynamic_reconfigure::IntParameter left_param;
+  dynamic_reconfigure::IntParameter right_param;
+  dynamic_reconfigure::IntParameter top_param;
+  dynamic_reconfigure::IntParameter bottom_param;
+
+  left_param.name = "left";
+  left_param.value = left;
+
+  right_param.name = "right";
+  right_param.value = right;
+
+  top_param.name = "top";
+  top_param.value = top;
+
+  bottom_param.name = "bottom";
+  bottom_param.value = bottom;
+
+  auto& config = req.config;
+  config.ints.push_back(left_param);
+  config.ints.push_back(right_param);
+  config.ints.push_back(top_param);
+  config.ints.push_back(bottom_param);
+
+  if (!ros::service::call(topic_depth_roi_, req, res))
+  {
+    ROS_ERROR("Failed to rconfigure the Depth auto exposure ROI");
+    return false;
+  }
+
+  return true;
 }
 
 }  // namespace vision
